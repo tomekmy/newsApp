f103c7ec278c3b42f6fc639f4fac7d2a63330a70
Tomasz Myśliwiec <tomasz.mysliwiec@execon.pl>
2023-10-27 18:13:25 +0200

    Add link to article

--- lib/src/news_feature/news_item_details_view.dart
+++ lib/src/news_feature/news_item_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/news_feature/news_item.dart';
-
+import 'package:news_app/src/utilities/utils.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class NewsItemDetailsView extends StatelessWidget {
   const NewsItemDetailsView(this.news, {super.key});
 
@@ -21,8 +22,17 @@ class NewsItemDetailsView extends StatelessWidget {
             Text(news.title),
             const SizedBox(height: 15,),
             Text(news.author),
-            const SizedBox(height: 10,),
-            Text(news.content),
+            if (news.content.isNotEmpty) ...[
+              const SizedBox(height: 10,),
+              Text(news.content),
+            ],
+            if (news.url.isNotEmpty) ...[
+              const SizedBox(height: 10,),
+              GestureDetector(
+                onTap: () => openUrl(news.url),
+                child: Text(AppLocalizations.of(context)!.linkToArticle)
+              ),
+            ],
           ],
         ),
       ),
